c39cf4f7c5307efcb3b8accf3d1051fed3a62fb9
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-10-10 18:20:44 +0000

    (fix) updated the deployment workflow to build properly

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true // mandatory, otherwise won't export
-  }
-}
-
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
 })
+
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
+}
 
 module.exports = withNextra(nextConfig)
