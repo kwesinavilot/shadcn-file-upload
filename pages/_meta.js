b8faee2ffbda197aea1a4a0481e53bb37f2739b5
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-10-10 15:54:59 +0000

    (feat) added the installation page

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,3 +1,12 @@
 export default {
-    index: 'Home',
+    index: {
+        title: 'Home',
+        description: 'A versatile and customizable file upload component for React, built with shadcn/ui and react-dropzone.',
+        link: '/'
+    },
+    installation: {
+        title: 'Installation',
+        description: 'Learn how to install and use the shadcn-file-upload component in your React project.',
+        link: '/installation'
+    }
 }
