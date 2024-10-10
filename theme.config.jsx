54e1687c2bd0171ede0c771e4d2fd2d43251b7b0
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-10-10 15:27:36 +0000

    (refa) restructured again to include nextra for docs site

---
+++ theme.config.jsx
@@ -0,0 +1,8 @@
+export default {
+    logo: <span>Shadcn File Upload</span>,
+    project: {
+        link: 'https://github.com/kwesinavilot/shadcn-file-upload',
+    },
+    docsRepositoryBase: 'https://github.com/kwesinavilot/shadcn-file-upload/tree/main/docs',
+    // ... other theme options
+}
