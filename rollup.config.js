db29a3e0d75bdf225d84f9c6920982e08bcb2805
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-10-10 14:44:28 +0000

    (refa) restructured and added new sections for package creation

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+export default {
+  input: 'src/FileUpload.jsx',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+  ],
+};
